55bcd84bffe8c081c3815cbe8779c5da844fac39
Masayuki Akiyama <okashimo23@gmail.com>
2013-08-11 11:29:47 +0900

    スタッフをテーブル保存に

--- app/Model/Staff.php
+++ app/Model/Staff.php
@@ -4,28 +4,45 @@ App::uses('AppModel', 'Model');
 class Staff extends AppModel
 {
   public $valid = array(
+    'add' => array(
+      'name'  => 'required | maxLen[50]',
+      'email' => 'required | maxLen[100]',
+    ),
   );
-  public $useTable = FALSE;
 
   public function auth($params)
   {
     if (empty($params['email'])) throw new Exception(__('Email is required.'));
     if (empty($params['password'])) throw new Exception(__('Password is required.'));
-    if ($params['email'] != MY_EMAIL || $params['password'] != MY_PASSWORD)
-      throw new Exception(__('Email or password is incorrect'));
 
-    $this->signIn();
+    $staff = $this->find('first', array(
+      CONDITIONS => array(
+        'Staff.email'    => $params['email'],
+        'Staff.password' => $this->hash($params['password']),
+      ),
+      FIELDS => array(
+        'Staff.id',
+      ),
+    ));
+    if (empty($staff)) throw new Exception(__('Email or password is incorrect'));
+
+    $this->signIn($staff['Staff']['id']);
 
     return TRUE;
   }
 
-  public function signIn()
+  public function signIn($staffId)
   {
-    $_SESSION['Staff'] = array(
-      'name'  => MY_NAME,
-      'email' => MY_EMAIL,
-      'editmode' => FALSE,
-    );
+    $staff = $this->find('first', array(
+      CONDITIONS => array(
+        'Staff.id' => $staffId,
+      ),
+      FIELDS => array(
+        'Staff.id', 'Staff.name', 'Staff.email',
+      ),
+    ));
+    $staff['Staff']['editmode'] = FALSE;
+    $_SESSION['Staff'] = $staff['Staff'];
   }
 
   public function signOut()
